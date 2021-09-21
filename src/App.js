3bb15b817637a88362b2219296337d1ae700d156
sinuhem23 <sinuhem23@gmail.com>
2021-09-21 15:22:18 -0400

    removed locaStorage for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
-import useLocalStorage from './Hooks/useLocalStorage';
+// import useLocalStorage from './Hooks/useLocalStorage';
 import './App.css';
 
 export default function App() {
   // Using useState
   // const [todo, setTodo] = useState([
   // Using LocalStorage instead of useState (check the localStorage file)
-  const [todo, setTodo] = useLocalStorage('todos', [
+  const [todo, setTodo] = useState('todos', [
     {
       defaultItem: 'Wake up',
       isChecked: true,
